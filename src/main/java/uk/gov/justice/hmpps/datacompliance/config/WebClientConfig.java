50921413236140f96026ff02e62cd38fd72a65a7
brightonsbox <jonathan.brighton@digital.justice.gov.uk>
2020-01-14 11:17:34 +0000

    GDPR-29 Adding health checks to elite2 and oauth (#22)
    * GDPR-29 Adding health checks to elite2 and oauth
    
    * GDPR-29 Added tests

--- src/main/java/uk/gov/justice/hmpps/datacompliance/config/WebClientConfig.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/config/WebClientConfig.java
@@ -1,5 +1,7 @@
 package uk.gov.justice.hmpps.datacompliance.config;
 
+import org.hibernate.validator.constraints.URL;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.*;
@@ -7,12 +9,33 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Validated
 @Configuration
 public class WebClientConfig {
 
-    @Bean
+    private final String elite2ApiBaseUrl;
+    private final String oauthApiBaseUrl;
+
+    public WebClientConfig(@Value("${elite2.api.base.url}") @URL final String elite2ApiBaseUrl,
+                           @Value("${oauth.api.base.url}") @URL final String oauthApiBaseUrl) {
+        this.elite2ApiBaseUrl = elite2ApiBaseUrl;
+        this.oauthApiBaseUrl = oauthApiBaseUrl;
+    }
+
+    @Bean(name = "elite2ApiHealthWebClient")
+    public WebClient elite2ApiHealthWebClient() {
+        return WebClient.create(elite2ApiBaseUrl);
+    }
+
+    @Bean(name = "oauthApiHealthWebClient")
+    public WebClient oauthApiHealthWebClient() {
+        return WebClient.create(oauthApiBaseUrl);
+    }
+
+    @Bean(name = "authorizedWebClient")
     WebClient authorizedWebClient(final OAuth2AuthorizedClientManager authorizedClientManager) {
 
         ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
---
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/services/health/HealthCheck.java
@@ -0,0 +1,51 @@
+package uk.gov.justice.hmpps.datacompliance.services.health;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static java.util.Objects.requireNonNull;
+import static lombok.AccessLevel.PROTECTED;
+
+@AllArgsConstructor(access = PROTECTED)
+abstract class HealthCheck implements HealthIndicator {
+
+    private final WebClient client;
+
+    @Override
+    public Health health() {
+        try {
+
+            ResponseEntity<String> response = client.get()
+                    .uri("/ping")
+                    .retrieve()
+                    .toEntity(String.class)
+                    .block();
+
+            return Health.up()
+                    .withDetail("HttpStatus", requireNonNull(response).getStatusCodeValue())
+                    .build();
+
+        } catch (final Exception ex) {
+            return Health.down(ex).build();
+        }
+    }
+
+    @Component
+    static class OAuthApiHealth extends HealthCheck {
+        protected OAuthApiHealth(@Qualifier("oauthApiHealthWebClient") final WebClient client) {
+            super(client);
+        }
+    }
+
+    @Component
+    static class Elite2ApiHealth extends HealthCheck {
+        protected Elite2ApiHealth(@Qualifier("elite2ApiHealthWebClient") final WebClient client) {
+            super(client);
+        }
+    }
+}
