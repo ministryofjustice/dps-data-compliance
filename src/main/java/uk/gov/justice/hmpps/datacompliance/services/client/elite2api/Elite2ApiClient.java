00d57a5aa8a7d0d4f25d8fd56f7cf96308881786
brightonsbox <jonathan.brighton@digital.justice.gov.uk>
2020-01-21 16:49:25 +0000

    GDPR-29 Adding tool for iterating over all offender numbers to perform an action (#26)

--- src/main/java/uk/gov/justice/hmpps/datacompliance/services/client/elite2api/Elite2ApiClient.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/services/client/elite2api/Elite2ApiClient.java
@@ -1,19 +1,17 @@
 package uk.gov.justice.hmpps.datacompliance.services.client.elite2api;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import uk.gov.justice.hmpps.datacompliance.config.DataComplianceProperties;
+import uk.gov.justice.hmpps.datacompliance.dto.OffenderNumber;
 
-import java.util.Set;
+import java.util.*;
 
 import static java.util.Objects.requireNonNull;
-import static java.util.stream.Collectors.toSet;
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 @Service
@@ -30,29 +28,46 @@ public class Elite2ApiClient {
         this.dataComplianceProperties = dataComplianceProperties;
     }
 
-    public Set<String> getOffenderNumbers(final long offset, final long limit) {
-        return getOffenderNumbersFlux(offset, limit).toStream().collect(toSet());
-    }
+    public OffenderNumbersResponse getOffenderNumbers(final long offset, final long limit) {
 
-    public Flux<String> getOffenderNumbersFlux(final long offset, final long limit) {
-
-        return webClient.get()
+        var response = webClient.get()
                 .uri(dataComplianceProperties.getElite2ApiBaseUrl() + OFFENDER_IDS_PATH)
                 .header("Page-Offset", String.valueOf(offset))
                 .header("Page-Limit", String.valueOf(limit))
                 .attributes(clientRegistrationId("dps-data-compliance"))
                 .retrieve()
-                .bodyToFlux(OffenderNumber.class)
-                .map(response -> requireNonNull(response.getOffenderNumber(),
-                        "Response contained null offender numbers"))
-                .distinct();
+                .toEntityList(OffenderNumber.class)
+                .block();
+
+        return offenderNumbersResponse(response);
     }
 
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    static class OffenderNumber {
-        @JsonProperty("offenderNumber")
-        private String offenderNumber;
+    private OffenderNumbersResponse offenderNumbersResponse(final ResponseEntity<List<OffenderNumber>> response) {
+
+        var offenderNumbers = requireNonNull(response.getBody(), "No body found in response.");
+
+        return OffenderNumbersResponse.builder()
+                .totalCount(getTotalCountFrom(response))
+                .offenderNumbers(new HashSet<>(offenderNumbers))
+                .build();
+    }
+
+    @SuppressWarnings("rawtypes")
+    private long getTotalCountFrom(final ResponseEntity entity) {
+
+        var totalCountHeader = Optional.ofNullable(entity.getHeaders())
+                .map(headers -> headers.get("Total-Records"))
+                .flatMap(headers -> headers.stream().findFirst());
+
+        return totalCountHeader
+                .map(Long::valueOf)
+                .orElseThrow(() -> new IllegalStateException("Response did not contain Total-Records header"));
+    }
+
+    @Data
+    @Builder
+    public static class OffenderNumbersResponse {
+        private long totalCount;
+        private Set<OffenderNumber> offenderNumbers;
     }
 }
---
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/services/migration/OffenderIterator.java
@@ -0,0 +1,48 @@
+package uk.gov.justice.hmpps.datacompliance.services.migration;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.justice.hmpps.datacompliance.dto.OffenderNumber;
+import uk.gov.justice.hmpps.datacompliance.services.client.elite2api.Elite2ApiClient;
+import uk.gov.justice.hmpps.datacompliance.services.client.elite2api.Elite2ApiClient.OffenderNumbersResponse;
+
+import java.util.function.Consumer;
+import java.util.stream.LongStream;
+
+import static java.lang.Math.ceil;
+
+@Slf4j
+@AllArgsConstructor
+public class OffenderIterator {
+
+    private final Elite2ApiClient elite2ApiClient;
+    private final long requestLimit;
+
+    public void applyForAll(final OffenderAction action) {
+
+        log.info("Applying offender action to first batch of {} offenders", requestLimit);
+        final var firstBatchResponse = applyForBatch(action, 0);
+
+        log.info("Applying offender action to a total of {} offenders", firstBatchResponse.getTotalCount());
+        LongStream.rangeClosed(1, indexOfFinalBatch(firstBatchResponse))
+                .forEach(batchIndex -> applyForBatch(action, batchIndex));
+
+        log.info("Offender action applied");
+    }
+
+    private OffenderNumbersResponse applyForBatch(final OffenderAction action, final long batchIndex) {
+
+        log.info("Applying offender action to batch {}", batchIndex);
+
+        final var response = elite2ApiClient.getOffenderNumbers(batchIndex * requestLimit, requestLimit);
+        response.getOffenderNumbers().forEach(action);
+        return response;
+    }
+
+    private long indexOfFinalBatch(final OffenderNumbersResponse response) {
+        return (long) ceil((double) response.getTotalCount() / requestLimit) - 1;
+    }
+
+    public interface OffenderAction extends Consumer<OffenderNumber> { }
+
+}
