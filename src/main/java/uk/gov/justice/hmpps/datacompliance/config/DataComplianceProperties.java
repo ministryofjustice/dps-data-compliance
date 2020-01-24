0fc1d6ab5a6b3acf02343ad0dd6540c755f0b064
brightonsbox <jonathan.brighton@digital.justice.gov.uk>
2020-01-24 15:36:10 +0000

    GDPR-29 Adding cron based upload of offender image data (#29)
    Work still outstanding:
     * Database-backed to track AWS Rekognition face ids
     * Periodic retrieval of new offender images

--- src/main/java/uk/gov/justice/hmpps/datacompliance/config/DataComplianceProperties.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/config/DataComplianceProperties.java
@@ -4,18 +4,21 @@ import lombok.Getter;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.annotation.Validated;
 
-import java.time.Duration;
-
-@Configuration
-@Validated
 @Getter
+@Validated
+@Configuration
+@EnableScheduling
 public class DataComplianceProperties {
 
     private final String elite2ApiBaseUrl;
+    private final long elite2ApiOffenderIdsLimit;
 
-    public DataComplianceProperties(@Value("${elite2.api.base.url}") @URL final String elite2ApiBaseUrl) {
+    public DataComplianceProperties(@Value("${elite2.api.base.url}") @URL final String elite2ApiBaseUrl,
+                                    @Value("${elite2.api.offender.ids.page.limit:100}") final long elite2ApiOffenderIdsLimit) {
         this.elite2ApiBaseUrl = elite2ApiBaseUrl;
+        this.elite2ApiOffenderIdsLimit = elite2ApiOffenderIdsLimit;
     }
 }
