c492900469ea40a02967c170d6dce7c500b36269
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-01-31 14:06:17 +0000

    GDPR-36 Using Quartz to run cron in a cluster safe way (#40)

--- src/main/java/uk/gov/justice/hmpps/datacompliance/config/AwsRekognitionConfig.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/config/AwsRekognitionConfig.java
@@ -4,25 +4,59 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import uk.gov.justice.hmpps.datacompliance.services.migration.OffenderImageMigrationJob;
 
+import static org.quartz.CronScheduleBuilder.cronSchedule;
+
+@Slf4j
 @Configuration
 public class AwsRekognitionConfig {
 
+    @Value("${image.recognition.migration.cron:@null}")
+    private String imageMigrationCron;
+
     @Bean
     @ConditionalOnProperty(name = "image.recognition.provider", havingValue = "aws")
-    AmazonRekognition amazonRekognition(@Value("${image.recognition.aws.access.key.id}") String accessKey,
-                                        @Value("${image.recognition.aws.secret.access.key}") String secretKey,
-                                        @Value("${image.recognition.region}") String region) {
+    AmazonRekognition amazonRekognition(@Value("${image.recognition.aws.access.key.id}") final String accessKey,
+                                        @Value("${image.recognition.aws.secret.access.key}") final String secretKey,
+                                        @Value("${image.recognition.region}") final String region) {
 
         final var credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return AmazonRekognitionClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "image.recognition.migration.cron")
+    public Trigger offenderImageMigrationTrigger() {
+
+        log.info("Configured to run offender image recognition migration with schedule: '{}'", imageMigrationCron);
+
+        return TriggerBuilder.newTrigger().forJob(offenderImageMigrationJobDetails())
+                .withIdentity("offender-image-migration-trigger")
+                .withSchedule(cronSchedule(imageMigrationCron))
+                .build();
+    }
+
+    @Bean
+    @Lazy
+    public JobDetail offenderImageMigrationJobDetails() {
+        return JobBuilder.newJob(OffenderImageMigrationJob.class)
+                .withIdentity("offender-image-migration-job")
+                .storeDurably()
                 .build();
     }
 }
--- src/main/java/uk/gov/justice/hmpps/datacompliance/services/migration/OffenderImageMigration.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/services/migration/OffenderImageMigration.java
@@ -1,9 +1,8 @@
 package uk.gov.justice.hmpps.datacompliance.services.migration;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.hmpps.datacompliance.model.ImageUploadBatch;
 import uk.gov.justice.hmpps.datacompliance.repository.ImageUploadBatchRepository;
@@ -13,6 +12,7 @@ import static com.microsoft.applicationinsights.agent.shadow.com.google.common.c
 
 @Slf4j
 @Service
+@AllArgsConstructor
 @ConditionalOnProperty(name = "image.recognition.migration.cron")
 class OffenderImageMigration {
 
@@ -21,21 +21,6 @@ class OffenderImageMigration {
     private final ImageUploadBatchRepository repository;
     private final TimeSource timeSource;
 
-    OffenderImageMigration(final OffenderIterator offenderIterator,
-                           final OffenderImageUploaderFactory uploaderFactory,
-                           final ImageUploadBatchRepository repository,
-                           final TimeSource timeSource,
-                           @Value("${image.recognition.migration.cron}") final String migrationCron) {
-
-        log.info("Configured to run offender image recognition migration with schedule: '{}'", migrationCron);
-
-        this.offenderIterator = offenderIterator;
-        this.uploaderFactory = uploaderFactory;
-        this.repository = repository;
-        this.timeSource = timeSource;
-    }
-
-    @Scheduled(cron = "${image.recognition.migration.cron}")
     void run() {
 
         log.info("Running offender image migration");
