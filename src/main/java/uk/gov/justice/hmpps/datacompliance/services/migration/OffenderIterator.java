0f2e54fa8ef7f30572baf0ebb0d00be33981e10f
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-02-03 09:23:28 +0000

    GDPR-37 Allowing iteration over a smaller subset of offenders (#41)

--- src/main/java/uk/gov/justice/hmpps/datacompliance/services/migration/OffenderIterator.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/services/migration/OffenderIterator.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.hmpps.datacompliance.services.migration;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.hmpps.datacompliance.config.DataComplianceProperties;
@@ -10,26 +11,28 @@ import uk.gov.justice.hmpps.datacompliance.services.client.elite2api.Elite2ApiCl
 import java.util.function.Consumer;
 import java.util.stream.LongStream;
 
+import static java.lang.Long.MAX_VALUE;
 import static java.lang.Math.ceil;
+import static java.lang.Math.min;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 class OffenderIterator {
 
     private final Elite2ApiClient elite2ApiClient;
-    private final long requestLimit;
-
-    OffenderIterator(final Elite2ApiClient elite2ApiClient, final DataComplianceProperties properties) {
-        this.elite2ApiClient = elite2ApiClient;
-        this.requestLimit = properties.getElite2ApiOffenderIdsLimit();
-    }
+    private final DataComplianceProperties properties;
 
     void applyForAll(final OffenderAction action) {
 
-        log.info("Applying offender action to first batch of up to {} offenders", requestLimit);
+        log.info("Applying offender action to first batch of up to {} offenders, offset: {}",
+                pageLimit(), properties.getElite2ApiOffenderIdsInitialOffset());
         final var firstBatchResponse = applyForBatch(action, 0);
 
         log.info("Total number of {} offenders", firstBatchResponse.getTotalCount());
+        properties.getOffenderIdsTotalPages()
+                .ifPresent(total -> log.info("Limiting iteration to {} pages of data", total));
+
         LongStream.rangeClosed(1, indexOfFinalBatch(firstBatchResponse))
                 .forEach(batchIndex -> applyForBatch(action, batchIndex));
 
@@ -40,13 +43,23 @@ class OffenderIterator {
 
         log.info("Applying offender action to batch {}", batchIndex);
 
-        final var response = elite2ApiClient.getOffenderNumbers(batchIndex * requestLimit, requestLimit);
+        final var offset = properties.getElite2ApiOffenderIdsInitialOffset() + (batchIndex * pageLimit());
+        final var response = elite2ApiClient.getOffenderNumbers(offset, pageLimit());
+
         response.getOffenderNumbers().forEach(action);
+
         return response;
     }
 
     private long indexOfFinalBatch(final OffenderNumbersResponse response) {
-        return (long) ceil((double) response.getTotalCount() / requestLimit) - 1;
+
+        final var totalBatches = (long) ceil((double) response.getTotalCount() / pageLimit());
+
+        return min(totalBatches, properties.getOffenderIdsTotalPages().orElse(MAX_VALUE)) - 1;
+    }
+
+    private long pageLimit() {
+        return properties.getElite2ApiOffenderIdsLimit();
     }
 
     interface OffenderAction extends Consumer<OffenderNumber> { }
