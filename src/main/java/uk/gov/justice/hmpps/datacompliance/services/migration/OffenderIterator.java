7fb5839a6ed808b569bb5ce1774b8b3b7e60dbae
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-03-02 19:02:14 +0000

    GDPR-26 Adding retry to image upload
    At the moment transient errors can cause the upload to halt.
    This will now provide an exponential back-off retry to the
    upload process.

--- src/main/java/uk/gov/justice/hmpps/datacompliance/services/migration/OffenderIterator.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/services/migration/OffenderIterator.java
@@ -1,16 +1,25 @@
 package uk.gov.justice.hmpps.datacompliance.services.migration;
 
+import com.google.common.annotations.VisibleForTesting;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.hmpps.datacompliance.config.DataComplianceProperties;
 import uk.gov.justice.hmpps.datacompliance.dto.OffenderNumber;
 import uk.gov.justice.hmpps.datacompliance.services.client.elite2api.Elite2ApiClient;
 import uk.gov.justice.hmpps.datacompliance.services.client.elite2api.Elite2ApiClient.OffenderNumbersResponse;
 
+import java.time.Duration;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.stream.LongStream;
 
+import static io.github.resilience4j.core.IntervalFunction.ofExponentialBackoff;
+import static io.github.resilience4j.retry.RetryConfig.custom;
 import static java.lang.Long.MAX_VALUE;
 import static java.lang.Math.ceil;
 import static java.lang.Math.min;
@@ -24,15 +33,32 @@ import static uk.gov.justice.hmpps.datacompliance.utils.Exceptions.propagateAnyE
 @Service
 class OffenderIterator {
 
+    private static final int UPLOAD_RETRY_MAX_ATTEMPTS = 5;
+    private static final Duration UPLOAD_RETRY_INITIAL_INTERVAL = Duration.ofMillis(500);
+    private static final double UPLOAD_RETRY_MULTIPLIER = 2;
+
     private final Elite2ApiClient elite2ApiClient;
     private final DataComplianceProperties properties;
     private final ExecutorService executorService;
+    private final RetryConfig retryConfig;
 
+    @Autowired
     public OffenderIterator(final Elite2ApiClient elite2ApiClient,
                             final DataComplianceProperties properties) {
+        this(elite2ApiClient, properties, custom()
+                .maxAttempts(UPLOAD_RETRY_MAX_ATTEMPTS)
+                .intervalFunction(ofExponentialBackoff(UPLOAD_RETRY_INITIAL_INTERVAL, UPLOAD_RETRY_MULTIPLIER))
+                .build());
+    }
+
+    @VisibleForTesting
+    OffenderIterator(final Elite2ApiClient elite2ApiClient,
+                     final DataComplianceProperties properties,
+                     final RetryConfig retryConfig) {
         this.executorService = newFixedThreadPool(properties.getElite2ApiOffenderIdsIterationThreads());
         this.elite2ApiClient = elite2ApiClient;
         this.properties = properties;
+        this.retryConfig = retryConfig;
     }
 
     void applyForAll(final OffenderAction action) {
@@ -58,7 +84,7 @@ class OffenderIterator {
         final var offset = properties.getElite2ApiOffenderIdsInitialOffset() + (batchIndex * pageLimit());
         final var response = elite2ApiClient.getOffenderNumbers(offset, pageLimit());
         final var tasks = response.getOffenderNumbers().stream()
-                .map(offenderNumber -> callable(() -> action.accept(offenderNumber)))
+                .map(offenderNumber -> applyWithRetry(action, offenderNumber))
                 .collect(toList());
 
         try {
@@ -89,4 +115,12 @@ class OffenderIterator {
 
     interface OffenderAction extends Consumer<OffenderNumber> { }
 
+    private Callable<Object> applyWithRetry(final OffenderAction offenderAction, final OffenderNumber offenderNumber) {
+        AtomicInteger i = new AtomicInteger();
+        return callable(() -> Retry.of(offenderNumber.getOffenderNumber(), retryConfig)
+                .executeRunnable(() -> {
+                    System.out.println(i.getAndIncrement());
+                    offenderAction.accept(offenderNumber);
+                }));
+    }
 }
