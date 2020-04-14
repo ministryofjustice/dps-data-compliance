29b5109984144f18ef4d0f2e69787314487dd261
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-04-14 13:05:28 +0100

    GDPR-62 Checking and recording Pathfinder referral

--- src/main/java/uk/gov/justice/hmpps/datacompliance/services/retention/RetentionService.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/services/retention/RetentionService.java
@@ -2,6 +2,7 @@ package uk.gov.justice.hmpps.datacompliance.services.retention;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import uk.gov.justice.hmpps.datacompliance.client.pathfinder.PathfinderApiClient;
 import uk.gov.justice.hmpps.datacompliance.dto.OffenderNumber;
 import uk.gov.justice.hmpps.datacompliance.repository.jpa.model.retention.RetentionReason;
 
@@ -11,19 +12,27 @@ import java.util.Optional;
 @AllArgsConstructor
 public class RetentionService {
 
+    private final PathfinderApiClient pathfinderApiClient;
     private final ManualRetentionService manualRetentionService;
 
     public Optional<RetentionReason> findRetentionReason(final OffenderNumber offenderNumber) {
 
         // TODO GDPR-51 complete the following checks
-        //  * Pathfinder
         //  * Duplicates
         //  * Moratoria
 
-        return manualRetentionService.findManualOffenderRetentionWithReasons(offenderNumber)
-                .map(manualRetention ->
-                        RetentionReason.builder()
-                                .manualRetention(manualRetention)
-                                .build());
+        final var isReferredToPathfinder = pathfinderApiClient.isReferredToPathfinder(offenderNumber);
+        final var manualRetention = manualRetentionService.findManualOffenderRetentionWithReasons(offenderNumber);
+
+        if (!isReferredToPathfinder && manualRetention.isEmpty()) {
+            return Optional.empty();
+        }
+
+        final var retentionReason = RetentionReason.builder()
+                .pathfinderReferred(isReferredToPathfinder);
+
+        manualRetention.ifPresent(retentionReason::manualRetention);
+
+        return Optional.of(retentionReason.build());
     }
 }
