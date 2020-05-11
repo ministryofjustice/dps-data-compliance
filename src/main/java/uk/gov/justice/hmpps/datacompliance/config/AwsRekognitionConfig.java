d6a7973ef07ff1afac6f8c4fc473d7aba6cc6d93
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-05-11 09:04:15 +0100

    GDPR-106 Fixing Quartz rescheduling (#135)

--- src/main/java/uk/gov/justice/hmpps/datacompliance/config/AwsRekognitionConfig.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/config/AwsRekognitionConfig.java
@@ -5,22 +5,29 @@ import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
 import uk.gov.justice.hmpps.datacompliance.jobs.imageupload.OffenderImageMigrationJob;
 
+import java.util.Set;
+
 import static org.quartz.CronScheduleBuilder.cronSchedule;
 
 @Slf4j
 @Configuration
 public class AwsRekognitionConfig {
+
+    @Autowired
+    private SchedulerFactoryBean schedulerFactoryBean;
 
     @Bean
     @ConditionalOnProperty(name = "image.recognition.provider", havingValue = "aws")
@@ -38,7 +45,15 @@ public class AwsRekognitionConfig {
 
     @Bean
     @ConditionalOnProperty(name = "image.recognition.migration.cron")
-    public Trigger offenderImageMigrationTrigger(@Value("${image.recognition.migration.cron}") final String imageMigrationCron) {
+    public ApplicationRunner rescheduleImageMigration(@Value("${image.recognition.migration.cron}") final String imageMigrationCron) {
+        return args -> schedulerFactoryBean.getScheduler()
+                .scheduleJob(
+                        offenderImageMigrationJobDetails(),
+                        Set.of(offenderImageMigrationTrigger(imageMigrationCron)),
+                        true);
+    }
+
+    public Trigger offenderImageMigrationTrigger(final String imageMigrationCron) {
 
         log.info("Configured to run offender image recognition migration with schedule: '{}'", imageMigrationCron);
 
@@ -48,8 +63,6 @@ public class AwsRekognitionConfig {
                 .build();
     }
 
-    @Bean
-    @Lazy
     public JobDetail offenderImageMigrationJobDetails() {
         return JobBuilder.newJob(OffenderImageMigrationJob.class)
                 .withIdentity("offender-image-migration-job")
