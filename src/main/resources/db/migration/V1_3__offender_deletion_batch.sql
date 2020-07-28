0f38e7575d08b12d2fff3851fa1283aa330358bd
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-07-28 12:11:20 +0100

    GDPR-168 Merging flyway scripts before deploying to production

--- src/main/resources/db/migration/V1_3__offender_deletion_batch.sql
+++ src/main/resources/db/migration/V1_3__offender_deletion_batch.sql
@@ -5,8 +5,11 @@ CREATE TABLE OFFENDER_DELETION_BATCH
   BATCH_ID                        BIGSERIAL       NOT NULL,
   REQUEST_DATE_TIME               TIMESTAMP       NOT NULL,
   REFERRAL_COMPLETION_DATE_TIME   TIMESTAMP,
-  WINDOW_START_DATE_TIME          TIMESTAMP       NOT NULL,
-  WINDOW_END_DATE_TIME            TIMESTAMP       NOT NULL,
+  WINDOW_START_DATE_TIME          TIMESTAMP,
+  WINDOW_END_DATE_TIME            TIMESTAMP,
+  REMAINING_IN_WINDOW             INTEGER,
+  BATCH_TYPE                      VARCHAR(255)    NOT NULL,
+  COMMENT_TEXT                    VARCHAR(4000),
 
   CONSTRAINT OFF_DEL_BAT_PK       PRIMARY KEY (BATCH_ID)
 );
@@ -18,3 +21,11 @@ COMMENT ON COLUMN OFFENDER_DELETION_BATCH.REQUEST_DATE_TIME IS 'The timestamp of
 COMMENT ON COLUMN OFFENDER_DELETION_BATCH.REFERRAL_COMPLETION_DATE_TIME IS 'Confirmation timestamp that all pending deletions have been added to the queue for this batch';
 COMMENT ON COLUMN OFFENDER_DELETION_BATCH.WINDOW_START_DATE_TIME IS 'Batches of deletions are conducted by selecting offenders that were due for deletion between two timestamps. This is the timestamp marking the start of this window.';
 COMMENT ON COLUMN OFFENDER_DELETION_BATCH.WINDOW_END_DATE_TIME IS 'Batches of deletions are conducted by selecting offenders that were due for deletion between two timestamps. This is the timestamp marking the end of this window.';
+COMMENT ON COLUMN OFFENDER_DELETION_BATCH.REMAINING_IN_WINDOW IS 'The number of offenders eligible for referral within the window that have not yet been referred';
+COMMENT ON COLUMN OFFENDER_DELETION_BATCH.BATCH_TYPE IS 'The type of the offender deletion (e.g. SCHEDULED or AD_HOC)';
+COMMENT ON COLUMN OFFENDER_DELETION_BATCH.COMMENT_TEXT IS 'An explanation of why the deletion was required';
+
+CREATE INDEX OFF_DEL_BAT_RDT_IDX ON OFFENDER_DELETION_BATCH(REQUEST_DATE_TIME);
+CREATE INDEX OFF_DEL_BAT_WSDT_IDX ON OFFENDER_DELETION_BATCH(WINDOW_START_DATE_TIME);
+CREATE INDEX OFF_DEL_BAT_WEDT_IDX ON OFFENDER_DELETION_BATCH(WINDOW_END_DATE_TIME);
+CREATE INDEX OFF_DEL_BAT_BT_IDX ON OFFENDER_DELETION_BATCH(BATCH_TYPE);
--- src/main/resources/db/migration/V1_7__manual_retention.sql
+++ src/main/resources/db/migration/V1_7__manual_retention.sql
@@ -5,9 +5,11 @@ CREATE TABLE MANUAL_RETENTION
   MANUAL_RETENTION_ID           BIGSERIAL       NOT NULL,
   OFFENDER_NO                   VARCHAR( 10)    NOT NULL,
   RETENTION_DATE_TIME           TIMESTAMP       NOT NULL,
-  STAFF_ID                      BIGINT          NOT NULL,
+  USER_ID                       VARCHAR(32)     NOT NULL,
+  RETENTION_VERSION             INTEGER         NOT NULL,
 
-  CONSTRAINT MAN_RET_PK         PRIMARY KEY (MANUAL_RETENTION_ID)
+  CONSTRAINT MAN_RET_PK         PRIMARY KEY (MANUAL_RETENTION_ID),
+  CONSTRAINT MAN_RET_OV_UNIQUE  UNIQUE (OFFENDER_NO, RETENTION_VERSION)
 );
 
 COMMENT ON TABLE MANUAL_RETENTION IS 'Records a new/updated request by a member of staff to retain an offender record';
@@ -15,6 +17,7 @@ COMMENT ON TABLE MANUAL_RETENTION IS 'Records a new/updated request by a member 
 COMMENT ON COLUMN MANUAL_RETENTION.MANUAL_RETENTION_ID IS 'Primary key id';
 COMMENT ON COLUMN MANUAL_RETENTION.OFFENDER_NO IS 'The NOMS offender number';
 COMMENT ON COLUMN MANUAL_RETENTION.RETENTION_DATE_TIME IS 'The timestamp of this retention creation/update';
-COMMENT ON COLUMN MANUAL_RETENTION.STAFF_ID IS 'The unique staff identifier of the member of staff that requested the retention';
+COMMENT ON COLUMN MANUAL_RETENTION.USER_ID IS 'ID of the user who created the record';
+COMMENT ON COLUMN MANUAL_RETENTION.RETENTION_VERSION IS 'Version number of the offender retention record';
 
 CREATE INDEX MAN_RET_ON_IDX ON MANUAL_RETENTION(OFFENDER_NO);
