776a09fa707b217133eaebafc509b1595ccb158e
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-07-28 11:17:04 +0100

    :recycle: GDPR-168 Merging flyway scripts before deploying to production

--- src/main/resources/db/migration/V2_26__offender_deletion_batch_update.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE OFFENDER_DELETION_BATCH ADD REMAINING_IN_WINDOW INTEGER;
-COMMENT ON COLUMN OFFENDER_DELETION_BATCH.REMAINING_IN_WINDOW IS 'The number of offenders eligible for referral within the window that have not yet been referred';
