6f7521182ca512c93149e4e4e3ac8a27f99589d4
hyderkhan <hyder.khan@digital.justice.gov.uk>
2021-01-27 13:11:14 +0000

    Add integration tests

--- src/testIntegration/java/uk/gov/justice/hmpps/datacompliance/QueueIntegrationTest.java
+++ src/testIntegration/java/uk/gov/justice/hmpps/datacompliance/QueueIntegrationTest.java
@@ -1,31 +1,23 @@
 package uk.gov.justice.hmpps.datacompliance;
 
+import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.model.GetQueueAttributesResult;
+import junit.framework.AssertionFailedError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
+
+import static org.awaitility.Awaitility.await;
 
 public class QueueIntegrationTest extends IntegrationTest {
-
-    public static final String APPROXIMATE_NUMBER_OF_MESSAGES = "ApproximateNumberOfMessages";
-
-
-    @Value("${data.compliance.response.sqs.queue.name}")
-    String sqsResponseQueueName;
-
-    @Value("${data.compliance.request.sqs.queue.name}")
-    String sqsRequestQueueName;
-
-    @Value("${sns.topic.arn}")
-    String snsQueueName;
-
 
     String sqsRequestClientQueueUrl;
     String sqsRequestDlqClientQueueUrl;
     String sqsResponseClientQueueUrl;
     String sqsResponseDlqClientQueueUrl;
+    MockJmsListener mockJmsListener;
 
     @PostConstruct
     public void initialiseQueueUrls() {
@@ -33,11 +25,41 @@ public class QueueIntegrationTest extends IntegrationTest {
         sqsRequestDlqClientQueueUrl = sqsRequestDlqClient.getQueueUrl(sqsRequestQueueName).getQueueUrl();
         sqsResponseClientQueueUrl = sqsResponseClient.getQueueUrl(sqsResponseQueueName).getQueueUrl();
         sqsResponseDlqClientQueueUrl = sqsResponseDlqClient.getQueueUrl(sqsResponseQueueName).getQueueUrl();
+        mockJmsListener = new MockJmsListener(sqsResponseClient, sqsRequestClient, sqsRequestClientQueueUrl);
     }
 
-
-    int getNumberOfMessagesCurrentlyOnQueue(String queueUrl, AmazonSQS client) {
-        final GetQueueAttributesResult queueAttributes = client.getQueueAttributes(queueUrl, List.of(APPROXIMATE_NUMBER_OF_MESSAGES));
-        return Integer.parseInt(queueAttributes.getAttributes().get(APPROXIMATE_NUMBER_OF_MESSAGES));
+    public void waitForPathFinderApiRequestTo(String url) {
+        await().until(() -> requestExists(url));
     }
+
+    public boolean requestExists(String url) {
+        try {
+            return pathfinderApiMock.takeRequest().getRequestUrl().toString().contains(url);
+        } catch (InterruptedException e) {
+            throw new AssertionFailedError();
+        }
+    }
+
+    @Autowired
+    @Qualifier("dataComplianceRequestSqsClient")
+    AmazonSQS sqsRequestClient;
+
+    @Autowired
+    @Qualifier("dataComplianceRequestSqsDlqClient")
+    AmazonSQS sqsRequestDlqClient;
+
+    @Autowired
+    @Qualifier("dataComplianceResponseSqsClient")
+    AmazonSQS sqsResponseClient;
+
+    @Autowired
+    @Qualifier("dataComplianceResponseSqsDlqClient")
+    AmazonSQS sqsResponseDlqClient;
+
+    @Value("${data.compliance.response.sqs.queue.name}")
+    String sqsResponseQueueName;
+
+    @Value("${data.compliance.request.sqs.queue.name}")
+    String sqsRequestQueueName;
+
 }
