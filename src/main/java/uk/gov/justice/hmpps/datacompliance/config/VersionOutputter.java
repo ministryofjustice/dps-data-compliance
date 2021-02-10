d9541d4e172032990e771703f1dda5cfd41ea23b
hyderkhan <hyder.khan@digital.justice.gov.uk>
2021-02-10 12:11:19 +0000

    GDPR-234: Application insights & suppress owasp vuln

--- src/main/java/uk/gov/justice/hmpps/datacompliance/config/VersionOutputter.java
+++ src/main/java/uk/gov/justice/hmpps/datacompliance/config/VersionOutputter.java
@@ -1,11 +1,9 @@
 package uk.gov.justice.hmpps.datacompliance.config;
 
-import com.microsoft.applicationinsights.extensibility.ContextInitializer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.info.BuildProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 
@@ -20,9 +18,5 @@ public class VersionOutputter {
     public void logVersionOnStartup() {
         log.info("Version {} started", buildProperties.getVersion());
     }
-
-    @Bean
-    public ContextInitializer versionContextInitializer() {
-        return telemetryContext -> telemetryContext.getComponent().setVersion(buildProperties.getVersion());
-    }
 }
+
