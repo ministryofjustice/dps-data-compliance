fb5a96337f376ceec62d2f8a71f7c174cfc1ff9e
hyderkhan <hyder.khan@digital.justice.gov.uk>
2021-04-21 11:20:40 +0100

    GDPR-269: Add review required integration tests

--- src/testMessageIntegrationWithReviewPeriod/java/uk/gov/justice/hmpps/datacompliance/ReviewRequiredMessagesIntegrationTest.java
+++ src/testMessageIntegrationWithReviewPeriod/java/uk/gov/justice/hmpps/datacompliance/ReviewRequiredMessagesIntegrationTest.java
@@ -1,30 +1,22 @@
 package uk.gov.justice.hmpps.datacompliance;
 
 import okhttp3.mockwebserver.MockResponse;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.http.HttpStatus;
 
 import java.util.Set;
 
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.request.Request.DATA_DUPLICATE_DB_CHECK;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.request.Request.DATA_DUPLICATE_ID_CHECK;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.request.Request.FREE_TEXT_MORATORIUM_CHECK;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.request.Request.OFFENDER_DELETION_GRANTED;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.request.Request.OFFENDER_RESTRICTION_CHECK;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.response.SqsResponseQueueFactory.forDataDuplicateDbResult;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.response.SqsResponseQueueFactory.forDataDuplicateIdResult;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.response.SqsResponseQueueFactory.forFreeTextSearchResult;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.response.SqsResponseQueueFactory.forOffenderDeletionCompleteResult;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.response.SqsResponseQueueFactory.forOffenderPendingDeletion;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.response.SqsResponseQueueFactory.forOffenderRestrictionResult;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.response.SqsResponseQueueFactory.forPendingDeletionReferralComplete;
-import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.response.SqsResponseQueueFactory.forProvisionalDeletionReferral;
+import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.request.Request.*;
+import static uk.gov.justice.hmpps.datacompliance.utils.queue.sqs.response.SqsResponseQueueFactory.*;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class ReviewRequiredMessagesIntegrationTest extends QueueIntegrationTest {
 
     @Test
+    @Order(1)
     public void shouldAllowReviewPeriodWhenReviewIsRequired() {
 
         final var offenderIdDisplay = "A1234PO";
@@ -98,6 +90,78 @@ public class ReviewRequiredMessagesIntegrationTest extends QueueIntegrationTest 
 
         waitUntilResponseQueueMessagesAreConsumed();
         waitUntilResolutionStatusIsPersisted(referralId, "DELETED");
+    }
+
+    @Test
+    @Order(2)
+    public void shouldRetainOffenderWhenARetentionCheckFromSecondIterationComesBackPositive() {
+
+        final var offenderIdDisplay = "B1234PO";
+        final var batch = persistNewBatch();
+
+        final var pendingDeletionReferralComplete = forPendingDeletionReferralComplete(sqsResponseClientQueueUrl, batch.getBatchId(), 1L, 1L);
+        final var offenderPendingDeletionResponse = forOffenderPendingDeletion(sqsResponseClientQueueUrl, batch.getBatchId(), offenderIdDisplay);
+
+        hmppsAuthMock.enqueue(mockTokenAuthenticationResponse());
+        pathfinderApiMock.enqueue(new MockResponse().setResponseCode(HttpStatus.NOT_FOUND.value()));
+        communityApiMock.enqueue(new MockResponse().setResponseCode(HttpStatus.NOT_FOUND.value()));
+
+        mockJmsListener.respondToRequestWith(Set.of(offenderPendingDeletionResponse, pendingDeletionReferralComplete));
+
+        waitForPathFinderApiRequestTo("/pathfinder/offender/B1234PO");
+
+        mockJmsListener.verifyMessageReceivedOfEventType(DATA_DUPLICATE_ID_CHECK);
+        final var dataDuplicateIdRetentionCheckId = mockJmsListener.getCheckId(DATA_DUPLICATE_ID_CHECK);
+        mockJmsListener.respondToRequestWith(Set.of(forDataDuplicateIdResult(sqsResponseClientQueueUrl, offenderIdDisplay, dataDuplicateIdRetentionCheckId)));
+
+        mockJmsListener.verifyMessageReceivedOfEventType(DATA_DUPLICATE_DB_CHECK);
+        final var dataDuplicateDbRetentionCheckId = mockJmsListener.getCheckId(DATA_DUPLICATE_DB_CHECK);
+        mockJmsListener.respondToRequestWith(Set.of(forDataDuplicateDbResult(sqsResponseClientQueueUrl, offenderIdDisplay, dataDuplicateDbRetentionCheckId)));
+
+        mockJmsListener.verifyMessageReceivedOfEventType(FREE_TEXT_MORATORIUM_CHECK);
+        final var freeTextMoratoriumCheckId = mockJmsListener.getCheckId(FREE_TEXT_MORATORIUM_CHECK);
+        mockJmsListener.respondToRequestWith(Set.of(forFreeTextSearchResult(sqsResponseClientQueueUrl, offenderIdDisplay, freeTextMoratoriumCheckId)));
+
+        mockJmsListener.verifyMessageReceivedOfEventType(OFFENDER_RESTRICTION_CHECK);
+        final var offenderRestrictionCheckId = mockJmsListener.getCheckId(OFFENDER_RESTRICTION_CHECK);
+        mockJmsListener.respondToRequestWith(Set.of(forOffenderRestrictionResult(sqsResponseClientQueueUrl, offenderIdDisplay, offenderRestrictionCheckId, false)));
+
+        mockJmsListener.verifyNoMessageReceivedOfEventType(OFFENDER_DELETION_GRANTED);
+
+        waitUntilResponseQueueMessagesAreConsumed();
+        waitUntilResolutionStatusIsPersisted(offenderIdDisplay, "PROVISIONAL_DELETION_GRANTED");
+
+        mockJmsListener.clearMessages();
+
+        hmppsAuthMock.enqueue(mockTokenAuthenticationResponse());
+        pathfinderApiMock.enqueue(new MockResponse().setResponseCode(HttpStatus.NOT_FOUND.value()));
+        communityApiMock.enqueue(new MockResponse().setResponseCode(HttpStatus.OK.value()));
+
+        final var referralId = offenderDeletionReferralRepository.findByOffenderNo(offenderIdDisplay).stream().findFirst().get().getReferralId();
+        final var provisionalDeletionReferral = forProvisionalDeletionReferral(sqsResponseClientQueueUrl, referralId, offenderIdDisplay);
+
+        mockJmsListener.respondToRequestWith(Set.of(provisionalDeletionReferral));
+
+        waitForPathFinderApiRequestTo("/pathfinder/offender/B1234PO");
+
+        mockJmsListener.verifyMessageReceivedOfEventType(DATA_DUPLICATE_ID_CHECK);
+        final var secondDataDuplicateIdRetentionCheckId = mockJmsListener.getCheckId(DATA_DUPLICATE_ID_CHECK);
+        mockJmsListener.respondToRequestWith(Set.of(forDataDuplicateIdResult(sqsResponseClientQueueUrl, offenderIdDisplay, secondDataDuplicateIdRetentionCheckId)));
+
+        mockJmsListener.verifyMessageReceivedOfEventType(DATA_DUPLICATE_DB_CHECK);
+        final var secondDataDuplicateDbRetentionCheckId = mockJmsListener.getCheckId(DATA_DUPLICATE_DB_CHECK);
+        mockJmsListener.respondToRequestWith(Set.of(forDataDuplicateDbResult(sqsResponseClientQueueUrl, offenderIdDisplay, secondDataDuplicateDbRetentionCheckId)));
+
+        mockJmsListener.verifyMessageReceivedOfEventType(FREE_TEXT_MORATORIUM_CHECK);
+        final var secondFreeTextMoratoriumCheckId = mockJmsListener.getCheckId(FREE_TEXT_MORATORIUM_CHECK);
+        mockJmsListener.respondToRequestWith(Set.of(forFreeTextSearchResult(sqsResponseClientQueueUrl, offenderIdDisplay, secondFreeTextMoratoriumCheckId)));
+
+        mockJmsListener.verifyMessageReceivedOfEventType(OFFENDER_RESTRICTION_CHECK);
+        final var secondOffenderRestrictionCheckId = mockJmsListener.getCheckId(OFFENDER_RESTRICTION_CHECK);
+        mockJmsListener.respondToRequestWith(Set.of(forOffenderRestrictionResult(sqsResponseClientQueueUrl, offenderIdDisplay, secondOffenderRestrictionCheckId, false)));
+
+        mockJmsListener.verifyNoMessageReceivedOfEventType(OFFENDER_DELETION_GRANTED);
+        waitUntilResolutionStatusIsPersisted(offenderIdDisplay, "RETAINED");
     }
 
 
